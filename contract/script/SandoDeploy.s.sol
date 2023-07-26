74f86727b28dd90b86698566561295dbb175bc4c
vclwei <vclwei@qq.com>
2023-07-26 17:25:21 +0800

    Modify Deploy Script deploy Method to broadcast Method

--- contract/script/SandoDeploy.s.sol
+++ contract/script/SandoDeploy.s.sol
@@ -4,13 +4,15 @@ pragma solidity ^0.8.15;
 import "foundry-huff/HuffDeployer.sol";
 import "forge-std/Script.sol";
 
-contract Deployer is Script {
+contract SandoDeploy is Script {
     
     // serachers
     function setUp() public {
     }
     function run() public{
-        address addr = HuffDeployer.deploy("sando");
+        address addr = HuffDeployer.broadcast("sando");
         console.log(address(addr));
     }
 }
+
+//forge script ./script/DeploySandwich.s.sol --rpc-url http://127.0.0.1:8545 --broadcast --private-key PRIVATE_KEY
